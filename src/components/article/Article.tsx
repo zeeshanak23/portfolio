c9362113b1f20c4dabcc46aff713ee79182f85f0
zeeshanak23 <zakhter449@gmail.com>
2024-04-03 12:18:32 +0530

    Made the minor changes

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -65,7 +65,7 @@ const Article: FC<{
               return (
                 <div
                   key={i}
-                  className='  h-[auto] w-[350px] rounded-lg bg-white md:max-h-[600px]  md:w-[700px] dark:bg-zinc-900'
+                  className='  h-[auto] w-[100%] rounded-lg bg-white md:max-h-[600px]  md:w-[700px] dark:bg-zinc-900'
                 >
                   <div
                     onMouseOver={() => {
@@ -120,8 +120,8 @@ const Article: FC<{
               );
             })}
           </div>
-          <div className='ml-2  w-[350px]  md:w-[400px] '>
-            <div className='flex flex-col gap-4 rounded-lg border border-[#eee] bg-white p-6 dark:bg-zinc-900'>
+          <div className='ml-2   md:w-[400px] '>
+            <div className='flex w-[400px] flex-col gap-4 rounded-lg border border-[#eee] bg-white p-6 dark:bg-zinc-900'>
               <div className='flex items-center'>
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
@@ -156,7 +156,7 @@ const Article: FC<{
               </div>
             </div>
 
-            <div className='mt-4 flex flex-col gap-4 rounded-lg border border-[#eee] bg-white p-6 md:mt-10 dark:bg-zinc-900'>
+            <div className='mt-4 flex w-[400px] flex-col gap-4 rounded-lg border border-[#eee] bg-white p-6 md:mt-10 dark:bg-zinc-900'>
               <div className='flex'>
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
